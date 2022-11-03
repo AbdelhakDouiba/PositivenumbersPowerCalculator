14a10ac4a549fccc370fe70972fc5e5bc21bd1ea
AbdelhakDouiba <117383171+AbdelhakDouiba@users.noreply.github.com>
2022-11-03 18:14:58 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("##### Welcome to the POWER CACULATOR ####\n");
+    
+    int number  , z , i ;
+    unsigned int power ;
+    printf("enter the number : ");
+    scanf("%d" , &number);
+
+    printf("the order of the power 'Mark well => positive intger only' : ");
+    scanf("%u" , &power);
+
+    i = ( power - 1 );
+    z = number ;
+
+    if (number != 0 && power != 0 )
+    {
+        for( i ; i != 0 ; i--)
+        {
+            number = number*z ;
+
+        }
+
+          printf("The result of %d^%d is : %d", z, power , number);
+    }
+    else {
+          printf("The result is : 1");
+         }
+
+
+    return 0;
+}
